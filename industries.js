f8a91656f01175df35148b4734193e13da8d876a
Aniket24G <94295506+Aniket24G@users.noreply.github.com>
2024-02-06 11:15:38 +0530

    Add files via upload

---
+++ industries.js
@@ -0,0 +1,42 @@
+const Industries = [
+    {
+        "id" : 1,
+        "name" : "Automobile" 
+    },
+    {
+        "id" : 2,
+        "name" : "Aerospace" 
+    },
+    {
+        "id" : 3,
+        "name" : "Construction" 
+    },
+    {
+        "id" : 4,
+        "name" : "Defence" 
+    },
+    {
+        "id" : 5,
+        "name" : "Medical" 
+    },
+    {
+        "id" : 6,
+        "name" : "Security" 
+    },
+    {
+        "id" : 7,
+        "name" : "Design" 
+    },
+    {
+        "id" : 8,
+        "name" : "Manufacturing" 
+    },
+    {
+        "id" : 9,
+        "name" : "Research & Development" 
+    },
+    {
+        "id" : 10,
+        "name" : "Pharmaceuticals" 
+    }
+]
